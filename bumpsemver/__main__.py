0c66145b3b6ee5606003ca5000b41cdc385cfdf5
Zhao Wang <zhaow.km@gmail.com>
2021-06-25 21:24:48 +0200

    [BUGFIX] added explicit error message when running with Python 2

--- bumpsemver/__main__.py
+++ bumpsemver/__main__.py
@@ -1,3 +1,9 @@
+import sys
+
+if sys.version_info[0] == 2:
+    print("Fatal: this application requires Python3. Please run it in a virtualenv to make sure \"python --version\" gives 3.x.y.")
+    sys.exit(2)
+
 from bumpsemver.cli import main
 
 main()

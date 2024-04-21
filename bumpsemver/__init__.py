53e7b52ef7442e5a0f9078c5eabca48ef661d476
Zhao Wang <zhaow.km@gmail.com>
2024-04-21 02:44:46 +0200

    build(repo): bumped version 2.0.0 → 2.1.0

--- bumpsemver/__init__.py
+++ bumpsemver/__init__.py
@@ -2,5 +2,5 @@
 A utility to simplify semver bumping for git repos.
 """
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 __title__ = "bumpsemver"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # ruff: noqa
 import setuptools
 
-version='2.0.0' # noqa
+version='2.1.0' # noqa
 
 setuptools.setup()
